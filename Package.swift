21346865bf5d61214dd25c68e66a58bb30773231
Amjad Tubasi <Eng.amjad.tubasi.at@gmail.com>
2023-12-20 19:37:46 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,27 @@
+// swift-tools-version: 5.4
 import PackageDescription
 
 let package = Package(
-    name: "Stylist"
+    name: "Stylist",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(name: "Stylist", targets: ["Stylist"]),
+    ],
+    targets: [
+        .target(name: "Stylist",
+                path: "Stylist",
+                exclude: ["Resources/Original",
+                          "Resources/README.md",
+                          "Resources/update_metadata.sh",
+                          "Info.plist"],
+                resources: [
+                    .process("Resources/PhoneNumberMetadata.json")
+                ]),
+        .testTarget(name: "Tests",
+                    dependencies: ["Stylist"],
+                    path: "Tests",
+                    exclude: ["Info.plist"])
+    ]
 )
